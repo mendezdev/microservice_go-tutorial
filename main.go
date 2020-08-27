ed5863813663bf03b17b0b988d5c8003afcfc50c
Pablo Mendez <mendez.developer@gmail.com>
2020-08-27 08:38:29 -0300

    first episode

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, rq *http.Request) {
+		log.Println("Hello world")
+		d, err := ioutil.ReadAll(rq.Body)
+		if err != nil {
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goobye world")
+	})
+	http.ListenAndServe(":9090", nil)
+}
